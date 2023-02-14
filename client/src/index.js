9205dad87639a7d8ba9b0f753fdb89c42848d3ad
elmatosz <matos-2011@live.com>
2023-02-14 18:37:24 -0500

    fixed a bug where the register box was the wrong size

--- client/src/index.js
+++ client/src/index.js
@@ -8,13 +8,6 @@ import { configureStore } from '@reduxjs/toolkit';
 import globalReducer from "state";
 import { Provider } from 'react-redux';
 
-// Application id key
-const APP_ID = 'Ndm94BGqYG2Q5XU6A55yKELCwe0lfavIGOrf3rBa';
-// JS Key
-const JS_KEY = 'lrxLndB0z5LOQ4JJZYkUGX1DdcJUXk2eaHoB7wSP';
-// Parse Server URL
-const SERVER_HOST_URL = 'https://parseapi.back4app.com/';
-
 //Initializing Parse
 Parse.initialize(APP_ID, JS_KEY);
 Parse.serverURL = SERVER_HOST_URL;
