807bbe530b53ec3d5a72d44b128f8afb92efd495
mymduncan5 <122941895+mymduncan5@users.noreply.github.com>
2023-02-21 14:34:22 -0500

    added a couple pages

---
+++ client/src/scenes/vehicles/index.jsx
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+
+const Vehicles = () => {
+    const Car = useSelector((state) => state.global.Car)
+
+    return(
+        <div>
+            <table>
+                <thead>
+                    <tr>
+                        <th>License</th>
+                        <th>Make</th>
+                        <th>Model</th>
+                        <th>Color</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {Car.map(x => {
+                        return(
+                            <tr key={x.carId}>
+                                <td>{x.license}</td>
+                                <td>{x.make}</td>
+                                <td>{x.model}</td>
+                                <td>{x.color}</td>
+                            </tr>
+                        )
+                    })}
+                </tbody>
+            </table>
+            <button>Add Vehicle</button>
+        </div>
+    )
+}
+
+export default Vehicles;
--- client/src/state/index.js
+++ client/src/state/index.js
@@ -6,7 +6,47 @@ const initialState = {
   mode: "dark",
   //The initial user will be null which means not logged in
   //This will change when the user logs in at back4app
-  User: new Parse.User()
+  //Added other fields needed to be used in order to fill
+  //other fields.  All null initially.
+  User: new Parse.User(),
+  //Car: new Parse.Car(),
+  //ParkedCars: new Parse.ParkedCars(),
+  //Tag: new Parse.Tag(),
+  //ParkingLot: new Parse.ParkingLot()
+  ParkedCars: [{
+    carId: "car1",
+    parkingLotId: "lot1",
+    timeEntered: "11:00:00",
+    timeExited: "16:00:00"
+  }, 
+  {
+    carId: "car2",
+    parkingLotId: "lot2",
+    timeEntered: "16:00:00",
+    timeExited: "20:00:00"
+  }],
+  Tag: {
+    tagId: "blah",
+    userId: "Jose",
+    expiration: "2024-05-29",
+    effective: "2020-09-10"
+  },
+  Car: [{
+    carId: "car1",
+    userId: "user1",
+    license: "apvjnirep",
+    make: "toyota",
+    model: "carolla",
+    color: "gray",
+  },
+  {
+    carId: "car2",
+    userId: "user1",
+    license: "aprouns",
+    make: "Buick",
+    model: "Century",
+    color: "Black",
+  }]
 };
 
 export const globalSlice =  createSlice({
